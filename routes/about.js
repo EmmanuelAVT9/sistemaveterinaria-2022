90734128a495bb8de716f9dfdb3a41e1e8fc96e4
EmmanuelAVT9 <alexsoge@gmail.com>
2022-06-08 22:21:02 -0500

    yo-Modificaciones en archivos

--- routes/about.js
+++ routes/about.js
@@ -3,14 +3,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/about', function(req, res, next) {
+router.get('/', function(req, res, next) {
 
   res.render('about',
   // Este es el View Model 
   {
     name: 'Emmanuel Vázquez',
     email: 'prueba@gmail.com',
-    url: 'www.itgam.com/rivalcoba'
+    url: 'www.itgam.com/emmanuelVazquez'
    });
 });
 
