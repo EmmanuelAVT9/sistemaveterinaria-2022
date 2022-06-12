7189b90189c378fed851f1757e580372b5f5bb28
EmmanuelAVT9 <alexsoge@gmail.com>
2022-06-12 01:02:59 -0500

    Adding Babel-watch

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', author: 'Emmanuel Vazquez' });
+    // render manda a renderizar (generar y entregar)
+  // la vista al cliente
+  // Calculando
+  let emojieDataset = ['💻', '👨‍💻', '🎈', '🎄','🦺','🎢','🎁','🚆','🌍','♥'];
+  let emojie = emojieDataset[Math.floor(Math.random() * emojieDataset.length)];
+  res.render('index',
+  // Este es el View-Model
+  { 
+    title: 'Express', 
+    author: 'Emmanuel Vazquez',
+    emojie
+  });
 });
 
 module.exports = router;
