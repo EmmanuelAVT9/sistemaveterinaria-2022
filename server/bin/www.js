e4c22f7888fd1edbcb9e6642d27090dc6f4e2d0e
EmmanuelAVT9 <alexsoge@gmail.com>
2022-06-11 23:21:58 -0500

    Creating server folder Se crea el folder server para separar back y front

--- server/bin/www.js
+++ server/bin/www.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-var app = require('../app');
+var app = require('../../app');
 var debug = require('debug')('proyecfinalpwpcii2022:server');
 var http = require('http');
 
