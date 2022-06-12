cc79e6e5df22a42c1efd3bb0391b4499351114e8
EmmanuelAVT9 <alexsoge@gmail.com>
2022-06-12 10:03:42 -0500

    Actualizando Webpack moviendo public

--- server/app.js
+++ server/app.js
@@ -39,7 +39,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //Middleware de archivos estáticos
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..','public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
