9211cb2cd1df0a1b8a8380da289c94e049d4a9f9
EmmanuelAVT9 <alexsoge@gmail.com>
2022-06-13 07:27:27 -0500

    S44 Procesando ideas de proyectos

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -13,9 +13,21 @@ const index = (req, res) => {
     res.render('projects/addProjectView', {});
     // TODO: Agregar codigo para agregar proyectos
   };
+
+// Procesa el formulario que Agrega ideas de proyectos
+// POST /projects/add
+const addPost = (req, res) => {
+  // Desestructurando la informacion
+  // del formulario
+  const { name, description } = req.body;
+  // Regresar un objeto con los datos
+  // obtenidos del formulario
+  res.status(200).json({ name, description });
+};
   
   // Exportando el controlador
   export default {
     index,
     add,
+    addPost,
   };
